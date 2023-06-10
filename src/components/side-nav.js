ac58c53d188fa4b9bf57c0470189c433f7a57010
Hung Do <hungdoworks@gmail.com>
2023-06-10 18:01:18 +0700

    Sidebar: Add CV section

--- src/components/side-nav.js
+++ src/components/side-nav.js
@@ -6,6 +6,7 @@ import Image from "next/image";
 export default function SideNavigation({ children }) {
     const sideNavRef = useRef(null);
     const contentRef = useRef(null);
+    const urlCV = "https://www.canva.com/design/DAFaUgAG5p4/lgEwsD-b-ImZBOI6pK6W-g/view";
 
     /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
     const openNav = () => {
@@ -19,6 +20,12 @@ export default function SideNavigation({ children }) {
         contentRef.current.style.transform = "translate(0, 0)";
     }
 
+    const handleGoToCV = (e) => {
+        e.preventDefault();
+
+        window.open(urlCV);
+    }
+
     return (
         <>
             <div className={styles.sideNav} ref={sideNavRef}>
@@ -28,6 +35,13 @@ export default function SideNavigation({ children }) {
                 <Link href="/workshops" onClick={closeNav}>Workshops</Link>
                 <Link href="/others" onClick={closeNav}>Others</Link>
                 <Link href="/contact" onClick={closeNav}>Contact</Link>
+                <Link 
+                    href="https://www.canva.com/"
+                    onClick={handleGoToCV}
+                >
+                    <span className="highlight">Curriculum Vitae</span>
+                    <Image src="/icons/ic_open_in_new.svg" alt="" width={20} height={16} />
+                </Link>
             </div>
 
             {/* Use any element to open the sidenav */}
