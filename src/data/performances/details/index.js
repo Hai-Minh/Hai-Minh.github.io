89f876b6cfb97691b57f46619c50eae6f8723d30
Hung Do <hungdoworks@gmail.com>
2023-10-27 01:37:36 +0700

    New Update

--- src/data/performances/details/index.js
+++ src/data/performances/details/index.js
@@ -2,12 +2,14 @@ import AWoman from './a-woman';
 import TamSinh from './tam-sinh';
 import Lego from './l-ego';
 import Cramped from './cramped';
+import MrNobody from './mr-nobody';
 
 const PerformanceData = {
     "a-woman": AWoman,
     "tam-sinh": TamSinh,
     'l-ego': Lego,
     'cramped': Cramped,
+    'mr-nobody': MrNobody,
 };
 
 export default PerformanceData;
---
+++ src/data/performances/details/mr-nobody.js
@@ -0,0 +1,81 @@
+import img0 from "../../../../public/images/performances/mr_nobody/img_0.jpg";
+import img1 from "../../../../public/images/performances/mr_nobody/img_1.jpg";
+import img2 from "../../../../public/images/performances/mr_nobody/img_2.jpg";
+import img3 from "../../../../public/images/performances/mr_nobody/img_3.jpg";
+import img4 from "../../../../public/images/performances/mr_nobody/img_4.jpg";
+import img5 from "../../../../public/images/performances/mr_nobody/img_5.jpg";
+import img6 from "../../../../public/images/performances/mr_nobody/img_6.jpg";
+import img7 from "../../../../public/images/performances/mr_nobody/img_7.jpg";
+import img8 from "../../../../public/images/performances/mr_nobody/img_8.jpg";
+import img9 from "../../../../public/images/performances/mr_nobody/img_9.jpg";
+import img10 from "../../../../public/images/performances/mr_nobody/img_10.jpg";
+import img11 from "../../../../public/images/performances/mr_nobody/img_11.jpg";
+import img12 from "../../../../public/images/performances/mr_nobody/img_12.jpg";
+import img13 from "../../../../public/images/performances/mr_nobody/img_13.jpg";
+import img14 from "../../../../public/images/performances/mr_nobody/img_14.jpg";
+
+const title = (
+  <span>
+    {`Mr Nobody`}
+    <br />
+    <span style={{ fontSize: "20px", fontStyle: "italic" }}>
+      {`They would be perfectly normal human`}
+    </span>
+  </span>
+);
+
+const videos = [];
+
+const images = [
+  img0,
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  img7,
+  img8,
+  img9,
+  img10,
+  img11,
+  img12,
+  img13,
+  img14,
+];
+
+const description = (
+  <>
+    <span style={{ whiteSpace: "pre-line" }}>
+      {`
+“Look at you then look at me
+It can be different, it can be the same
+Maybe we are all the same…”
+
+
+The performance is the expression of sympathy besides an effort to share about injustices or power imbalances in many countries nowadays. Some people have lost their speech, some people have lost their right, some people simply cannot have a “normal” life!
+You are invited to come, to see, to feel and to breathe with the atmosphere of “Mr Nobody” that Hai was trying to create, with earnestness and care. 
+
+
+By and with: Minh Hải Trần
+Music: Tam Thi Pham
+Text: The Graveyard Book by Neil Gaiman
+With kind support of the residency program “Frankfurt Moves!”
+“Frankfurt Moves!” is an initiative of the foundation KfW Stiftung aimed at promoting artistic exchange and intercultural dialogue. The residency program in cooperation with the Frankfurt LAB supports international emerging artists in the performing arts.
+
+
+Record on 14th, October, 2023, Frankfurt LAB by De-Da Productions
+`}
+    </span>
+  </>
+);
+
+const credits = <></>;
+
+export default {
+  title,
+  videos,
+  description,
+  credits,
+  images,
+};
