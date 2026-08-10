b9afd5d2e24839e23e994fad64fe9e3dc9e42f06
hungdoworks <hungdoworks@gmail.com>
2026-08-10 14:08:37 +0700

    feat: add new item:'Chan Troi Goc Be'

--- src/data/index.js
+++ src/data/index.js
@@ -1,7 +1,9 @@
 import CocKienZoi from './coc-kien-zoi';
+import ChanTroiGocBe from './chan-troi-goc-be';
 
 const PerformancesData = {
     "coc-kien-zoi": CocKienZoi,
+    "chan-troi-goc-be": ChanTroiGocBe
 };
 
 export default PerformancesData;
