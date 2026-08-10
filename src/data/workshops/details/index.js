d127a405d4fc2e702506e39adf2ce45b0f58dd0a
hungdoworks <hungdoworks@gmail.com>
2026-08-10 14:43:42 +0700

    feat: add Moving With Space project

--- src/data/workshops/details/index.js
+++ src/data/workshops/details/index.js
@@ -1,11 +1,13 @@
 import ReleaseAndConnect from './release-and-connect';
 import ConnectAndCreative from './connect-and-creative';
 import BodyToHeart from './body-to-heart';
+import MovingWithSpace from './moving-with-space';
 
 const WorkshopData = {
     "release-and-connect": ReleaseAndConnect,
     "connect-and-creative": ConnectAndCreative,
     "body-to-heart": BodyToHeart,
+    "moving-with-space": MovingWithSpace,
 };
 
 export default WorkshopData;
