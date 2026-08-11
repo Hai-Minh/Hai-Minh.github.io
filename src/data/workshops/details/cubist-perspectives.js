dfea934635b42834ad72f5ccbc58765e80adc9f6
hungdoworks <hungdoworks@gmail.com>
2026-08-11 10:54:06 +0700

    feat: add 'Cubist Perspectives' workshop

---
+++ src/data/workshops/details/cubist-perspectives.js
@@ -0,0 +1,43 @@
+const title = "Journey Through The Art World: Cubist Perspectives";
+
+
+const videos = [];
+
+const images = [
+  "/images/cubist-perspectives/image_0.jpg",
+  "/images/cubist-perspectives/image_1.jpg",
+  "/images/cubist-perspectives/image_2.jpg",
+  "/images/cubist-perspectives/image_3.jpg",
+  "/images/cubist-perspectives/image_4.jpg",
+  "/images/cubist-perspectives/image_5.jpg",
+  "/images/cubist-perspectives/image_6.jpg"
+];
+
+const description = (
+  <>
+    <p>
+        {`In collaboration with Hoa Tam Art Center`}
+    </p>
+    <p style={{ paddingLeft: "24px" }}>
+      <ul>
+        <li><strong>Role:</strong> Movement Facilitator / Instructor</li>
+        <li><strong>Target Audience:</strong> Children</li>
+        <li><strong>Overview:</strong> An interdisciplinary workshop exploring Cubism through the integration of <strong>somatic movement</strong> and <strong>visual arts</strong>. Facilitated by artist Minh Hai, the movement exercises stimulate children's multi-sensory awareness, encouraging them to freely shift their physical perspectives in space (near/far, high/low, front/back) to observe how shapes transform. Guided by artist Din (Hoa Tam Art Center) in drawing, the children translate these lived spatial observations into unique, multi-dimensional Cubist artworks.</li>
+      </ul>
+    </p>
+  </>
+);
+
+
+const credits = (
+  <>
+  </>
+);
+
+export default {
+    title,
+    videos,
+    description,
+    credits,
+    images,
+}
